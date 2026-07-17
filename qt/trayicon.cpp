0eefc0f7e962d862938905a27565c3120849a039
agolzarniya4213-star <a.golzarniya4213@gmail.com>
2026-07-17 21:14:44 +0330

    Update trayicon.cpp

--- qt/trayicon.cpp
+++ qt/trayicon.cpp
@@ -2,7 +2,6 @@
 #include "ipcclient.h"
 #include <QApplication>
 #include <QStyle>
-#include <QMessageBox>
 
 TrayIcon::TrayIcon(QObject *parent)
     : QObject(parent)
@@ -10,38 +9,40 @@ TrayIcon::TrayIcon(QObject *parent)
     , m_menu(new QMenu())
     , m_connectAction(new QAction(tr("Connect"), this))
     , m_disconnectAction(new QAction(tr("Disconnect"), this))
-    , m_separator(new QAction(this))
-    , m_aboutAction(new QAction(tr("About Kingo VPN"), this))
+    , m_showAction(new QAction(tr("Show Window"), this))
     , m_quitAction(new QAction(tr("Quit"), this))
     , m_ipcClient(new IpcClient(this))
 {
     setupUI();
 
-    // Trigger Async IPC Commands
-    connect(m_connectAction, &QAction::triggered, [this]() {
-        m_connectAction->setEnabled(false); // Prevent double-clicks
+    // Strict Signal-Slot connections (Modern Qt5 syntax)
+    connect(m_trayIcon, &QSystemTrayIcon::activated, 
+            this, &TrayIcon::handleActivated);
+            
+    connect(m_connectAction, &QAction::triggered, this, [this]() {
         m_ipcClient->sendCommand("connect");
     });
     
-    connect(m_disconnectAction, &QAction::triggered, [this]() {
-        m_disconnectAction->setEnabled(false); // Prevent double-clicks
+    connect(m_disconnectAction, &QAction::triggered, this, [this]() {
         m_ipcClient->sendCommand("disconnect");
     });
+    
+    connect(m_quitAction, &QAction::triggered, 
+            this, &TrayIcon::quitRequested);
 
-    connect(m_aboutAction, &QAction::triggered, this, &TrayIcon::showAboutDialog);
-    connect(m_quitAction, &QAction::triggered, this, &TrayIcon::quitRequested);
-
-    // Handle Async IPC Responses safely
+    // IPC Response Handlers
     connect(m_ipcClient, &IpcClient::commandSuccess, this, [this](const QString &msg) {
-        bool isNowConnected = !m_connectAction->isEnabled() && m_disconnectAction->isEnabled();
-        if (!isNowConnected) {
-            onConnectSuccess(msg);
-        } else {
+        // Determine state based on which action was taken
+        bool isNowConnected = !m_connectAction->isEnabled();
+        if (isNowConnected) {
             onDisconnectSuccess(msg);
+        } else {
+            onConnectSuccess(msg);
         }
     });
     
-    connect(m_ipcClient, &IpcClient::commandError, this, &TrayIcon::onCommandError);
+    connect(m_ipcClient, &IpcClient::commandError, 
+            this, &TrayIcon::onCommandError);
 
     m_trayIcon->show();
 }
@@ -52,23 +53,43 @@ TrayIcon::~TrayIcon() {
 
 void TrayIcon::setupUI()
 {
-    m_separator->setSeparator(true);
-    
-    m_menu->addAction(m_connectAction);
-    m_menu->addAction(m_disconnectAction);
-    m_menu->addAction(m_separator);
-    m_menu->addAction(m_aboutAction);
-    m_menu->addAction(m_quitAction);
-
-    m_disconnectAction->setEnabled(false);
-
     m_trayIcon->setIcon(QApplication::style()->standardIcon(QStyle::SP_ComputerIcon));
     m_trayIcon->setToolTip("Kingo VPN - Disconnected");
     m_trayIcon->setContextMenu(m_menu);
+
+    m_menu->addAction(m_connectAction);
+    m_menu->addAction(m_disconnectAction);
+    m_menu->addSeparator();
+    m_menu->addAction(m_showAction);
+    m_menu->addAction(m_quitAction);
+
+    // Initial State Logic
+    m_disconnectAction->setEnabled(false);
 }
 
-void TrayIcon::updateTrayState(bool isConnected)
+void TrayIcon::handleActivated(QSystemTrayIcon::ActivationReason reason)
 {
+    // Reserved for future single-click logic
+    if (reason == QSystemTrayIcon::Trigger) {
+        // TODO: Toggle main window visibility
+    }
+}
+
+void TrayIcon::onConnectSuccess(const QString &message) {
+    updateVisualState(true);
+    m_trayIcon->showMessage("Kingo VPN", message, QSystemTrayIcon::Information, 3000);
+}
+
+void TrayIcon::onDisconnectSuccess(const QString &message) {
+    updateVisualState(false);
+    m_trayIcon->showMessage("Kingo VPN", message, QSystemTrayIcon::Information, 3000);
+}
+
+void TrayIcon::onCommandError(const QString &errorMessage) {
+    m_trayIcon->showMessage("Kingo VPN Error", errorMessage, QSystemTrayIcon::Critical, 5000);
+}
+
+void TrayIcon::updateVisualState(bool isConnected) {
     if (isConnected) {
         m_trayIcon->setIcon(QApplication::style()->standardIcon(QStyle::SP_DialogApplyButton));
         m_trayIcon->setToolTip("Kingo VPN - Connected");
@@ -80,24 +101,4 @@ void TrayIcon::updateTrayState(bool isConnected)
         m_connectAction->setEnabled(true);
         m_disconnectAction->setEnabled(false);
     }
-}
-
-void TrayIcon::onConnectSuccess(const QString &message) {
-    updateTrayState(true);
-    m_trayIcon->showMessage("Kingo VPN", message, QSystemTrayIcon::Information, 3000);
-}
-
-void TrayIcon::onDisconnectSuccess(const QString &message) {
-    updateTrayState(false);
-    m_trayIcon->showMessage("Kingo VPN", message, QSystemTrayIcon::Information, 3000);
-}
-
-void TrayIcon::onCommandError(const QString &message) {
-    updateTrayState(false); // Reset UI state on error
-    m_trayIcon->showMessage("Connection Error", message, QSystemTrayIcon::Critical, 5000);
-}
-
-void TrayIcon::showAboutDialog() {
-    QMessageBox::information(nullptr, "About Kingo VPN", 
-                             "Kingo Linux VPN Client\nVersion: 1.0.0\nArchitecture: Native Qt/Go IPC");
 }
