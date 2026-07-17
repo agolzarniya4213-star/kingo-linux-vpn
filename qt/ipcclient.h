19362520de44e434cac25947b84b0b4fa4bc4bd2
agolzarniya4213-star <a.golzarniya4213@gmail.com>
2026-07-17 21:01:08 +0330

    Update ipcclient.h

--- qt/ipcclient.h
+++ qt/ipcclient.h
@@ -9,16 +9,23 @@ class IpcClient : public QObject
     Q_OBJECT
 public:
     explicit IpcClient(QObject *parent = nullptr);
-
+    
+    // Async command sender (Does NOT freeze the UI)
     void sendCommand(const QString &action);
 
 signals:
     void commandSuccess(const QString &message);
     void commandError(const QString &message);
 
+private slots:
+    void onConnected();
+    void onReadyRead();
+    void onError(QLocalSocket::LocalSocketError socketError);
+
 private:
     QLocalSocket *m_socket;
     QString m_socketPath;
+    QString m_pendingAction;
 };
 
 #endif // IPCCLIENT_H
