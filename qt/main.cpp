5643ec934b343a1b1907dab46355cddbd2a213b5
agolzarniya4213-star <a.golzarniya4213@gmail.com>
2026-07-17 20:58:55 +0330

    Update main.cpp

--- qt/main.cpp
+++ qt/main.cpp
@@ -1,15 +1,25 @@
 #include <QApplication>
+#include <QStyle>
 #include "trayicon.h"
 
 int main(int argc, char *argv[]) {
-    // Prevents app from closing when hidden
+    // Set Application Metadata for Wayland/Linux Desktop integration
+    QCoreApplication::setApplicationName("Kingo VPN");
+    QCoreApplication::setApplicationVersion("1.0.0");
+    QCoreApplication::setOrganizationName("Kingo");
+    
+    // Critical fix for Wayland tray icon support
+    qputenv("QT_WAYLAND_DESKTOP_FILE_NAME", "kingo-linux-vpn-tray.desktop");
+
     QApplication app(argc, argv);
+    
+    // Prevents app from closing when menus are closed
     app.setQuitOnLastWindowClosed(false);
 
     // Initialize Tray Icon
     TrayIcon tray;
 
-    // Make the "Quit" button actually close the application
+    // Safe Quit connection
     QObject::connect(&tray, &TrayIcon::quitRequested, &app, &QApplication::quit);
 
     return app.exec();
